3602dde4ce266ebee4b603da22d09bcf8fff3571
Даниил Алексеенко <cra1ger@bk.ru>
2023-06-11 20:44:46 +0300

    test font

--- backend/foodgram/api/utils/list_to_pdf.py
+++ backend/foodgram/api/utils/list_to_pdf.py
@@ -1,19 +1,15 @@
 import io
-import os
 
 from django.http import FileResponse
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 
-from foodgram.settings import BASE_DIR
-
 
 def list_to_pdf(data):
     buffer = io.BytesIO()
     p = canvas.Canvas(buffer)
-    ttf_path = os.path.join(BASE_DIR, 'static/data/', 'arial.ttf')
-    pdfmetrics.registerFont(TTFont('Arial', ttf_path))
+    pdfmetrics.registerFont(TTFont('Arial', './arial.ttf'))
     p.setFont("Arial", 20)
     p.drawString(250, 800, "Список покупок:")
     height = 760
@@ -25,4 +21,4 @@ def list_to_pdf(data):
     p.save()
     buffer.seek(0)
     return FileResponse(buffer, as_attachment=True,
-                        filename="Список покупок.pdf")
+                        filename="Shopping_list.pdf")
