63ebed95c38b2e023d268e7534c1a23fb029bdf9
Даниил Алексеенко <cra1ger@bk.ru>
2023-06-12 13:12:42 +0300

    settings update

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -104,9 +104,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'SEARCH_PARAM': 'name',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
-    'SEARCH_PARAM': 'name',
 }
 
 DJOSER = {
